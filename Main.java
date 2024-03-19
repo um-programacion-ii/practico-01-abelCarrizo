010566263ac569ae13a98f61db402c054efff3c3
Abel Carrizo <aa.carrizo@alumno.um.edu.ar>
2024-03-19 19:41:00 -0300

    Entidad Main creada

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String args[]) {
+        //Se crea una instancia
+        Ingrediente ingrediente1 = new Ingrediente("Sal", 10);
+
+        //Se muestra en pantalla
+        System.out.println("Ingrediente 1: " + ingrediente1);
+    }
+}
