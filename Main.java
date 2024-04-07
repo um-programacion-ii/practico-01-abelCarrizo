2378ce5c29956e99e0516bc5ab29a4cff77e90ee
abelCarrizo <aa.carrizo@alumno.um.edu.ar>
2024-04-06 23:51:20 -0300

    Archivos removidos del lugar

--- Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String args[]) {
-        //Se crea una instancia
-        Ingrediente ingrediente1 = new Ingrediente("Sal", 10);
-
-        //Se muestra en pantalla
-        System.out.println("Ingrediente 1: " + ingrediente1);
-    }
-}
